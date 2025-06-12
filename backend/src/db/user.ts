d21fb8fd5f56316c5b1a664490c8076b38c50836
iUnstable0 <me@iunstable0.com>
2025-06-12 21:17:42 +0700

    i hate prisma

--- backend/src/db/user.ts
+++ backend/src/db/user.ts
@@ -1,13 +1,11 @@
 import validator from "validator";
 
 import prisma from "@/db/prisma";
-import lib_sanitize from "@/modules/sanitize.ts";
+import lib_vet from "@/modules/vet";
 
 export default class db_user {
   public static async getUserByEmail(email: string): Promise<any> {
-    const sanitizedEmail = lib_sanitize.email(email);
-
-    console.log(`Fetching user with email: ${sanitizedEmail}`);
+    const sanitizedEmail = lib_vet.email(email);
 
     return prisma.user.findUnique({
       where: { email: sanitizedEmail },
--- backend/src/gql/resolvers/mutation/user.ts
+++ backend/src/gql/resolvers/mutation/user.ts
@@ -1,10 +1,11 @@
-import { GraphQLError } from "graphql";
+import Bun from "bun";
 import validator from "validator";
+import { GraphQLError } from "graphql";
 
 import db_user from "@/db/user";
 
 import lib_captcha from "@/modules/captcha";
-import lib_sanitize from "@/modules/sanitize.ts";
+import lib_vet from "@/modules/vet";
 
 export default class mutation_user {
   public static async register(args: any, context: any) {
@@ -14,22 +15,22 @@ export default class mutation_user {
       throw new GraphQLError("Captcha verification failed", {
         extensions: {
           code: "BAD_REQUEST",
-          http: { status: 400 },
-        },
+          http: { status: 400 }
+        }
       });
     }
 
     const captchaValid = await lib_captcha.verify(
       captchaToken,
-      context.request.headers["CF-Connecting-IP"] || null,
+      context.request.headers["CF-Connecting-IP"] || null
     );
 
     if (!captchaValid) {
       throw new GraphQLError("Captcha verification failed", {
         extensions: {
           code: "BAD_REQUEST",
-          http: { status: 400 },
-        },
+          http: { status: 400 }
+        }
       });
     }
 
@@ -37,34 +38,70 @@ export default class mutation_user {
       throw new GraphQLError("Missing required fields", {
         extensions: {
           code: "BAD_REQUEST",
-          http: { status: 400 },
-        },
+          http: { status: 400 }
+        }
       });
     }
 
     let sanitizedEmail;
 
     try {
-      sanitizedEmail = lib_sanitize.email(email);
+      sanitizedEmail = lib_vet.email(email);
     } catch (error) {
       throw new GraphQLError("Invalid email", {
         extensions: {
           code: "BAD_REQUEST",
-          http: { status: 400 },
-        },
+          http: { status: 400 }
+        }
       });
     }
 
-    console.log(sanitizedEmail);
+    const nameVetResult = lib_vet.name(name);
 
-    console.log(await db_user.getUserByEmail(sanitizedEmail));
+    if (!nameVetResult.valid) {
+      throw new GraphQLError(nameVetResult.errors.join(". "), {
+        extensions: {
+          code: "BAD_REQUEST",
+          http: { status: 400 }
+        }
+      });
+    }
 
-    throw new GraphQLError("Not implemented", {
-      extensions: {
-        code: "NOT_IMPLEMENTED",
-        http: { status: 501 },
-      },
+    const passwordVetResult = lib_vet.password(password);
+
+    if (!passwordVetResult.valid) {
+      throw new GraphQLError(passwordVetResult.errors.join(". "), {
+        extensions: {
+          code: "BAD_REQUEST",
+          http: { status: 400 }
+        }
+      });
+    }
+
+    if (await db_user.getUserByEmail(sanitizedEmail)) {
+      throw new GraphQLError("Email already registered", {
+        extensions: {
+          code: "BAD_REQUEST",
+          http: { status: 400 }
+        }
+      });
+    }
+
+    const hashedPassword = await Bun.password.hash(password, {
+      algorithm: "argon2id"
     });
+
+    db_user.create({
+      email: sanitizedEmail,
+      name:
+    });
+
+    // throw new GraphQLError("Not implemented", {
+    //   extensions: {
+    //     code: "NOT_IMPLEMENTED",
+    //     http: { status: 501 },
+    //   },
+    // });
 
     // if (await db_user.getUserByEmail(sanitizedEmail)) {
     //   throw new GraphQLError("Email already registered", {
