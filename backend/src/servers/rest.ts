f455d1679d6fbcad623803c4d0103c354b37d775
iUnstable0 <me@iunstable0.com>
2025-07-09 21:23:49 +0700

    ugh

--- backend/src/servers/rest.ts
+++ backend/src/servers/rest.ts
@@ -22,7 +22,7 @@ const CORS_HEADERS = {
   headers: {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "OPTIONS, POST",
-    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization",
   },
 };
 
